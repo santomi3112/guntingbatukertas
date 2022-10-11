0b66fac01312814fb919c405ef99dd254894b629
santomi3112 <adiprasetio007@gmail.com>
2022-10-11 14:59:31 +0700

    suatu saat akan bersinar

--- script.js
+++ script.js
@@ -15,4 +15,48 @@ const SELECTIONS = [
         beats: 'batu'
     }
 ];
+const finalColumn = document.querySelector('[data-final-column]')
+const selectionButton = document.querySelectorAll('[data-selection]')
+const botScoreSpan = document.querySelector('[data-bot-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 
+selectionButton.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    const botSelection = randomSelection()
+    const yourWinner = isWinner(selection, botSelection)
+    const botWinner = isWinner(botSelection, selection)
+    
+    addSelectionResult(botSelection, botWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (botWinner) incrementScore(botScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult (selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
