034ab1b1a194d23c2f569d80c72340680c2a1431
santomi3112 <adiprasetio007@gmail.com>
2022-10-11 14:00:23 +0700

    added selections array

---
+++ script.js
@@ -0,0 +1,18 @@
+const SELECTIONS = [
+    {
+        name: 'batu',
+        emoji: '✊',
+        beats: 'gunting'
+    },
+    {
+        name: 'gunting',
+        emoji: '🖖',
+        beats: 'kertas'
+    },
+    {
+        name: 'kertas',
+        emoji: '✋',
+        beats: 'batu'
+    }
+];
+
