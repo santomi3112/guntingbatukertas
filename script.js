56b9fe8a0073c715349f8641c8f7181dc22e5235
santomi3112 <adiprasetio007@gmail.com>
2022-10-12 15:50:56 +0700

    almost done

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ const SELECTIONS = [
         beats: 'batu'
     }
 ];
-const finalColumn = document.querySelector('[data-final-column]')
-const selectionButton = document.querySelectorAll('[data-selection]')
-const botScoreSpan = document.querySelector('[data-bot-score]')
+
+const selectionButton = document.querySelectorAll('[data-selection]');
 const yourScoreSpan = document.querySelector('[data-your-score]')
+const botScoreSpan = document.querySelector('[data-bot-score]')
+const yourResult = document.querySelector('[data-your-result]')
+const botResult = document.querySelector('[data-bot-result]')
 
 selectionButton.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
@@ -28,35 +30,24 @@ selectionButton.forEach(selectionButton => {
     })
 })
 
-function makeSelection(selection) {
-    const botSelection = randomSelection()
-    const yourWinner = isWinner(selection, botSelection)
-    const botWinner = isWinner(botSelection, selection)
-    
-    addSelectionResult(botSelection, botWinner)
-    addSelectionResult(selection, yourWinner)
-
-    if (yourWinner) incrementScore(yourScoreSpan)
-    if (botWinner) incrementScore(botScoreSpan)
-}
-
-function incrementScore(scoreSpan) {
-    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-}
-
-function addSelectionResult (selection, winner) {
-    const div = document.createElement('div')
-    div.innerText = selection.emoji
-    div.classList.add('result-selection')
-    if (winner) div.classList.add('winner')
-    finalColumn.after(div)
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
 
 function isWinner(selection, opponentSelection) {
     return selection.beats === opponentSelection.name
 }
 
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS[randomIndex]
+function makeSelection(selection) {
+    const botSelection = randomSelection()
+    const yourWinner = isWinner(selection, botSelection)
+    const botWinner = isWinner(botSelection, selection)
+    
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (botWinner) incrementScore(botScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
 }
